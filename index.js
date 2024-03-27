2e15592675a40844c39fd5097a552a44d9590161
abdullateefyusuf <yusufolusesiabdullateef@gmail.com>
2024-03-27 22:55:16 +0100

    js-first-learn

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("meAbout").innerHTML="My name is Abdullateef yusuf";
+document.getElementById("learn").innerHTML="Am learnig at OIC HUB";
+document.getElementById("aboutLearn").innerHTML="Am good with all tutor at OIC HUB";
+
+
+document.write("She is at home");
+
+
+function motion() {
+    document.getElementById("lorryMe").innerHTML="No Struggle No Success";
+    document.getElementById("lorryMe").style.color="blue";
+}
